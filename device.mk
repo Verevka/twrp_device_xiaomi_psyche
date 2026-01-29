0bdc68fcf6f8b79e0900a437fba5913642bbf7df
HDzungx <hdzungx@gmail.com>
2026-01-29 16:42:44 +0700

    test: use qti aidl vibrator for haptic

--- device.mk
+++ device.mk
@@ -66,3 +66,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
+
+
+# Vibrator
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
