0d9e9e4a598e8097f944a0c8b05b8767068be8ee
Verevka <verevka86@gmail.com>
2026-02-01 16:05:37 +0300

    Fix fox_14.1

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Configure gsi_keys.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
